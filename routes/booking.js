58f45bc3cb7ec5e797b2ccbd43c5e0ab87f004b5
3abglil <mks.abogalil666@gmail.com>
2023-06-06 04:37:06 +0300

    loc

--- routes/booking.js
+++ routes/booking.js
@@ -8,24 +8,24 @@ router.get("/", async function (req, res, next) {
 });
 
 
-router.get("/:class", async function (req, res, next) {
-  let x = req.params.class;
-  console.log(x);
-    fs.readFile(`../travinature/public/countries/${x}.svg`, 'utf8', (err, data) => {
-        if (err) throw err;
-        // console.log(data); // Log the SVG file contents to the console
-        res.render("country",{data});
-      });
-  // res.render("home",{ user: (req.session.user === undefined ? "" : req.session.user) });
-});
+// router.get("/:class", async function (req, res, next) {
+//   let x = req.params.class;
+//   console.log(x);
+//     fs.readFile(`../travinature/public/countries/${x}.svg`, 'utf8', (err, data) => {
+//         if (err) throw err;
+//         // console.log(data); // Log the SVG file contents to the console
+//         res.render("country",{data});
+//       });
+//   // res.render("home",{ user: (req.session.user === undefined ? "" : req.session.user) });
+// });
 
 
 
 
 
-// router.get("/argentina", function (req, res, next) {
-//     res.render("argentina",{ user: (req.session.user === undefined ? "" : req.session.user) });
-//   });
+router.get("/argentina", function (req, res, next) {
+    res.render("argentina",{ user: (req.session.user === undefined ? "" : req.session.user) });
+  });
 
 
 export default router;
